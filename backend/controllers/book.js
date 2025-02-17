8a810eb14846b45153dd6c7cd50766786788f8c2
Justin Acheaibi <justin.acheaibi@gmail.com>
2025-02-17 07:29:40 +0100

    modification des code d'erreur et de la version de certaines dépendances

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -15,7 +15,7 @@ exports.createBook = (req, res, next) => {
 
   book.save()
   .then(() => { res.status(201).json({message: 'Livre enregistré !'})})
-  .catch(error => { res.status(400).json( { error })})
+  .catch(error => { res.status(500).json( { error })})
 };
 
 exports.modifyBook = (req, res, next) => {
@@ -28,15 +28,15 @@ exports.modifyBook = (req, res, next) => {
   Book.findOne({_id: req.params.id})
       .then((book) => {
           if (book.userId != req.auth.userId) {
-              res.status(401).json({ message : 'Not authorized'});
+              res.status(403).json({ message : 'Not authorized'});
           } else {
               Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
               .then(() => res.status(200).json({message : 'Livre modifié!'}))
-              .catch(error => res.status(401).json({ error }));
+              .catch(error => res.status(500).json({ error }));
           }
       })
       .catch((error) => {
-          res.status(400).json({ error });
+          res.status(500).json({ error });
       });
 };
 
@@ -44,13 +44,13 @@ exports.deleteBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id})
       .then(book => {
           if (book.userId != req.auth.userId) {
-              res.status(401).json({message: 'Not authorized'});
+              res.status(403).json({message: 'Not authorized'});
           } else {
               const filename = book.imageUrl.split('/images/')[1];
               fs.unlink(`images/${filename}`, () => {
                   Book.deleteOne({_id: req.params.id})
                       .then(() => { res.status(200).json({message: 'Livre supprimé !'})})
-                      .catch(error => res.status(401).json({ error }));
+                      .catch(error => res.status(500).json({ error }));
               });
           }
       })
